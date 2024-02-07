e33ffdf1a4c6284341403d3a005f182b46e4e607
Evan Cooke <evncooke@gmail.com>
2024-02-06 19:36:25 -0600

    Updated LoginSignup Page
    Added option to choose between student or practitioner on sign up page. Results in different text fields being displayed depending on which picture/option is selected.

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './LoginSignup.css';
 
-import user_icon from '../Assets/person.png'
-import email_icon from '../Assets/email.png'
-import password_icon from '../Assets/password.png'
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+import student_icon from '../Assets/graduation-cap.png';
+import practitioner_icon from '../Assets/training-icon.png';
 
 const LoginSignup = () => {
 
-    const [action,setAction] = useState("Sign Up");
+    const [action,setAction] = useState("Login");
+    const [role,setRole] = useState("");
 
     return (
         <div className='container'>
@@ -15,11 +18,26 @@ const LoginSignup = () => {
                 <div className='text'>{action}</div>
                 <div className='underline'></div>
             </div>
+            {action==="Login"?null:<div className='role-selection'>
+                <div className={`role ${role === "student" ? "selected" : ""}`} onClick={()=>{setRole("student")}}>
+                    <img src={student_icon} alt="" />
+                    <div>Student</div>
+                </div>
+                <div className={`role ${role === "practitioner" ? "selected" : ""}`} onClick={()=>{setRole("practitioner")}}>
+                    <img src={practitioner_icon} alt="" />
+                    <div>Practitioner</div>
+                </div>
+            </div>}
             <div className='inputs'>
-                {action==="Login"?<div></div>:<div className='input'>
+                {action==="Login"?null:<div className='input'>
                     <img src={user_icon} alt="" />
                     <input type="text" placeholder="Name" />
                 </div>}
+
+                {role==="student"?<div className='input'>
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder="School" />
+                </div>:null}
                 
                 <div className='input'>
                     <img src={email_icon} alt="" />
@@ -32,7 +50,7 @@ const LoginSignup = () => {
             </div>
             {action==="Sign Up"?<div></div>:<div className='forgot-password'>Lost Password? <span>Click Here</span></div>}
             <div className='submit-container'>
-                <div className={action==="Login"?"sumbit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
                 <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
             </div>
         </div>
