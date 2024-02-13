eef1f8b83745f0a67f68e09b22923aac2b7d1a5d
Evan Cooke <evncooke@gmail.com>
2024-02-12 19:19:29 -0600

    Initial Commit
    Added header bar to student homepage

---
+++ src/Components/PractitionerHomepage/PractitionerHomepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PractitionerHomepage() {
+    return (
+        <div>
+            <h1>Practitioner Homepage</h1>
+        </div>
+    )
+}
+
+export default PractitionerHomepage;
---
+++ src/Components/StudentHomepage/StudentHomepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './StudentHomepage.css';
+
+function StudentHomepage() {
+    return (
+        <div>
+            <div className='header-bar'>
+                <h2>Student Homepage</h2>
+            </div>
+        </div>
+    )
+}
+
+export default StudentHomepage;
