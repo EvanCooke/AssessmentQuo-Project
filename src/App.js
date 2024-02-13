a631dee92b33c22a1efe718166413019bf0e2415
Evan Cooke <evncooke@gmail.com>
2024-02-12 19:18:26 -0600

    Added routers for page navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import LoginSignup from './Components/LoginSignup/LoginSignup.jsx';
+import StudentHomepage from './Components/StudentHomepage/StudentHomepage.jsx';
+import PractitionerHomepage from './Components/PractitionerHomepage/PractitionerHomepage.jsx';
+import Error from './Components/Error/Error.jsx';
 
 function App() {
     return (
-        <div>
-            <LoginSignup/>
-        </div>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<LoginSignup/>} />
+                <Route exact path="/student-home" element={<StudentHomepage/>} />
+                <Route exact path="/practitioner-home" element={<PractitionerHomepage/>} />
+                <Route exact path="*" element={<Error/>} />
+            </Routes>            
+        </Router>
     );
 }
 
