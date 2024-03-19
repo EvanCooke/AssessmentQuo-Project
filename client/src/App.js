14f2cd192700f6b4e145b665f9751d5a83cf59cc
EmSheshan <emsheshan@gmail.com>
2024-03-18 22:03:50 -0500

    added bug fixes Signed-off-by: EmSheshan <emsheshan@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+// import statements:
+
 import React from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -24,5 +26,26 @@ function App() {
         */
     )
 }
+
+/*
+function App() {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+        /**
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<LoginSignup/>} />
+                <Route exact path="/student-home" element={<StudentHomepage/>} />
+                <Route exact path="/practitioner-home" element={<PractitionerHomepage/>} />
+                <Route exact path="*" element={<Error/>} />
+            </Routes>            
+        </Router>
+        */
+    )
+}
+
+*/
 
 export default App;
