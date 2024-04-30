6df194cfe8cee9b6a5c68f95967d20434fc25406
Evan Cooke <evcooke@uiowa.edu>
2024-04-29 21:06:53 -0500

    Switched from absolute units to relative units

--- client/src/Components/Homepage/Homepage.jsx
+++ client/src/Components/Homepage/Homepage.jsx
@@ -27,18 +27,19 @@ function Homepage() {
                 </div>
             </div>
             <div className={styles.secondContainer}>
-                <img className={styles.starsBackground} src={stars_background} alt="" />
-                <div className={styles.ourMission}>OUR MISSION</div>
-                <div className={styles.ourMissionText}>
-                    <p>Welcome to our platform, where we believe in the power of education and the potential of technology.</p>
-                    <p>Our mission is to simplify the learning process for both educators and students.</p>
-                    <br />
-                    <p>We provide educators with a comprehensive tool to analyze test data,</p>
-                    <p>enabling them to tailor their teaching methods to individual student needs.</p>
-                    <p>For students, we offer an accessible platform to take diagnostic assessments,</p>
-                    <p>fostering a personalized and effective learning experience.</p>
-                    <br />
-                    <p>Together, we are shaping the future of education, one test at a time.</p>
+                <div className={styles.starsBackground}>
+                    <div className={styles.ourMission}>OUR MISSION</div>
+                    <div className={styles.ourMissionText}>
+                        <p>Welcome to our platform, where we believe in the power of education and the potential of technology.</p>
+                        <p>Our mission is to simplify the learning process for both educators and students.</p>
+                        <br />
+                        <p>We provide educators with a comprehensive tool to analyze test data,</p>
+                        <p>enabling them to tailor their teaching methods to individual student needs.</p>
+                        <p>For students, we offer an accessible platform to take diagnostic assessments,</p>
+                        <p>fostering a personalized and effective learning experience.</p>
+                        <br />
+                        <p>Together, we are shaping the future of education, one test at a time.</p>
+                    </div>
                 </div>
             </div>
         </div>
